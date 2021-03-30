03154a21d92fd83d9c901a93beb0eff033f9750a
Anton Ralovets <by.ralovets@gmail.com>
2021-03-30 22:28:41 +0300

    Make DB in-cloud

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/yelab';
+// const url = 'mongodb://localhost:27017/yelab';
+const url = 'mongodb+srv://harry_potter:F60nnNRFFru1LIVK@cluster0.rivuz.mongodb.net/deadline-tracker?retryWrites=true';
 
-mongoose.connect(url);
+mongoose.connect(url, { useNewUrlParser: true });
 
 module.exports = mongoose;
